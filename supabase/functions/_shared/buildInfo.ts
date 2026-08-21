859ad5581578f5a9149d004e081d730cfd5ad695
prasidha sarawagi <kathmanducreatives@gmail.com>
2026-08-21 23:00:32 +0545

    chore: deploy stamp for f2fb01c6

--- supabase/functions/_shared/buildInfo.ts
+++ supabase/functions/_shared/buildInfo.ts
@@ -2,7 +2,7 @@
 // Do not hand-edit. Regenerate by deploying.
 
 export const BUILD_INFO = {
-  git_sha: "71ee16dcd72d9f391cdc26fc8f4cc20b04cbe269",
-  build_timestamp: "2026-08-21T16:58:27.725Z",
+  git_sha: "f2fb01c63cb3d48c839f210837a43c1e7004abf2",
+  build_timestamp: "2026-08-21T17:15:07.268Z",
   dirty: false,
 } as const;
